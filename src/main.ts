0e7446cc34e5bf1e66bd14fb112be218761b543f
lweislak <150990328+lweislak@users.noreply.github.com>
2024-10-13 00:11:24 -0700

    Step 3 (Added helper function and automatic clicking)

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,32 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Saltiness Level";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//Create button (Step 1)
+//Create Button (Step 1)
 const button = document.createElement("button");
 app.append(button);
 button.innerText = "🧂"; //Salt shaker emoji
 
-//Increment (Step 2)
+//Helper function that updates the button text
+function updateButton() {
+  button.innerText = `🧂 Levels Increased By x ${counter}`;
+}
+
+//Increment Counter (Step 2)
 let counter: number = 0;
 button.addEventListener("click", function () {
   counter += 1;
-  button.innerText = `🧂 Shaken ${counter} Times`;
+  updateButton();
 });
+
+//Automatic Clicking (Step 3)
+const intervalID: number = window.setInterval(() => {
+  counter += 1;
+  updateButton();
+}, 1000);
