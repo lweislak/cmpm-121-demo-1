f47ed81e2d934ee9943434d0fdf9c4758c734750
lweislak <150990328+lweislak@users.noreply.github.com>
2024-10-16 00:59:10 -0700

    Step 5 (Needs fix)

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,44 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//Create Button (Step 1)
+
+//Create Main Button (Step 1)
 const button = document.createElement("button");
 app.append(button);
 button.innerText = "🧂"; //Salt shaker emoji
 
-//
+//Create Upgrade Button (Step 5)
+const upgrade = document.createElement("button");
+upgrade.innerText = "Play a match of League of Legends";
+upgrade.style.position = "absolute";
+upgrade.style.left = "50%";
+upgrade.style.transform = "translateX(50%)";
+app.append(upgrade);
+upgrade.disabled = true; //Start with button disabled
+
+
+function checkUpgradeButton() {
+  if(counter >= 10.0) {
+    upgrade.disabled = false; //Show button is counter is over 10
+    checkUpgradeClick();
+  } else {
+    upgrade.disabled = true;
+  }
+}
+
+//TOFIX: This function is being called multiple times
+        //So it deducts counter more than once
+//If button is shown, check for a click
+function checkUpgradeClick() {
+  upgrade.addEventListener("click", function () {
+    requestAnimationFrame(step.bind(performance.now()));
+    counter -= 10.0;
+    updateButton();
+    console.log("Counter:", counter);
+  });
+  upgrade.removeEventListener("click", function() {});
+}
+
 
 //Increment Counter (Step 2)
 let counter: number = 0.0;
@@ -28,10 +60,10 @@ function updateButton() {
   //Code for rounding decimal places found at: https://stackoverflow.com/a/5623156
   const count = counter.toFixed(2).replace(/\.00$/, ""); //Rounds to the hundredths place
   button.innerText = `🧂 Levels Increased By x ${count}`;
+  checkUpgradeButton();
 }
 
-requestAnimationFrame(step.bind(performance.now()));
-
+//Allows for gradual growth of the counter value
 function step(prevTime: number) {
   const currTime = performance.now();
   const seconds = (currTime - prevTime) / 1000; //Convert from milliseconds to seconds
