227dec63f7b2325f2a250fdc5ed9397e63bf22ef
lweislak <150990328+lweislak@users.noreply.github.com>
2024-10-16 16:48:42 -0700

    Step 5

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
 //Create Main Button (Step 1)
 const button = document.createElement("button");
 app.append(button);
@@ -24,29 +23,21 @@ upgrade.style.transform = "translateX(50%)";
 app.append(upgrade);
 upgrade.disabled = true; //Start with button disabled
 
-
 function checkUpgradeButton() {
-  if(counter >= 10.0) {
-    upgrade.disabled = false; //Show button is counter is over 10
-    checkUpgradeClick();
+  if (counter >= 10.0) {
+    upgrade.disabled = false; //Show button if counter is over 10
   } else {
     upgrade.disabled = true;
   }
 }
 
-//TOFIX: This function is being called multiple times
-        //So it deducts counter more than once
 //If button is shown, check for a click
-function checkUpgradeClick() {
-  upgrade.addEventListener("click", function () {
-    requestAnimationFrame(step.bind(performance.now()));
-    counter -= 10.0;
-    updateButton();
-    console.log("Counter:", counter);
-  });
-  upgrade.removeEventListener("click", function() {});
-}
-
+upgrade.addEventListener("click", function () {
+  requestAnimationFrame(step.bind(performance.now()));
+  counter -= 10.0;
+  updateButton();
+  console.log("Counter:", counter); //TEST
+});
 
 //Increment Counter (Step 2)
 let counter: number = 0.0;
@@ -61,6 +52,7 @@ function updateButton() {
   const count = counter.toFixed(2).replace(/\.00$/, ""); //Rounds to the hundredths place
   button.innerText = `🧂 Levels Increased By x ${count}`;
   checkUpgradeButton();
+  console.log("TEST");
 }
 
 //Allows for gradual growth of the counter value
