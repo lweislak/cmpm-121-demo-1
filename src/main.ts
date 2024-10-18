80300d292848eb783895156380ba4a6f3a91b469
lweislak <150990328+lweislak@users.noreply.github.com>
2024-10-18 01:48:47 -0700

    Step 9

--- src/main.ts
+++ src/main.ts
@@ -9,68 +9,57 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-//Create Main Button (Step 1)
+//Create main button (Step 1)
 const button = document.createElement("button");
 app.append(button);
 button.innerText = "🧂"; //Salt shaker emoji
 
-//Create Cost 10 Upgrade Button (Step 5)
-const numberOfClicks = [0, 0, 0];
-const cost = [10, 100, 1000];
 
-const upgrade10 = document.createElement("button");
-upgrade10.innerText = `Play a match of Overwatch\nCost: ${cost[0]}`;
-setupButton(upgrade10, "translateX(65%)");
+//Create upgrade buttons
+const Overwatch = document.createElement("button");
+Overwatch.innerText = `Play a match of Overwatch\nCost: 10`;
+setupButton(Overwatch, "translateX(65%)"); //Move button
 
-//Create Cost 100 Upgrade Button (Step 6)
-const upgrade100 = document.createElement("button");
-upgrade100.innerText = `Play a match of Valorant\nCost: ${cost[1]}`;
-setupButton(upgrade100, "translateX(-170%)");
+const Valorant = document.createElement("button");
+Valorant.innerText = `Play a match of Valorant\nCost: 100`;
+setupButton(Valorant, "translateX(-170%)"); //Move button
 
-//Create Cost 1,000 Upgrade Button (Step 6)
-const upgrade1k = document.createElement("button");
-upgrade1k.innerText = `Play a match of League of Legends\nCost: ${cost[2]}`;
-setupButton(upgrade1k, "translateY(200%)");
+const LoL = document.createElement("button");
+LoL.innerText = `Play a match of League of Legends\nCost: 1000`;
+setupButton(LoL, "translateY(200%)"); //Move button
 
-//Increment Counter (Step 2)
+
+interface Item {
+  name: string,
+  button: HTMLButtonElement,
+  cost: number,
+  rate: number,
+  clicks: number
+};
+
+const avaliableItems: Item[] = [
+  {name: "Overwatch", button: Overwatch, cost: 10, rate: 0.1, clicks: 0},
+  {name: "Valorant", button: Valorant, cost: 100, rate: 2.0, clicks: 0},
+  {name: "League of Legends", button: LoL, cost: 1000, rate: 50, clicks: 0},
+];
+
+//Increment button counter
 let counter: number = 0.0;
 button.addEventListener("click", function () {
   counter += 1.0;
   updateButton();
 });
 
-//If button is shown, check for a click
-//TODO: Condense into function
-upgrade10.addEventListener("click", function () {
-  requestAnimationFrame(step.bind(performance.now()));
-  counter -= 10.0;
-  numberOfClicks[0]++;
-  cost[0] *= 1.15; //Increase cost for upgrade
-  upgrade10.innerText = `Play a match of Overwatch\nCost: ${roundHundredths(cost[0])}`;
-  updateButton();
-});
-upgrade100.addEventListener("click", function () {
-  //Change growth rate. This is janky
-  for (let i = 0; i < 100; i++) {
+//Setup event listener that checks for a click if button is active
+for (const item of avaliableItems) {
+  item.button.addEventListener("click", function () {
     requestAnimationFrame(step.bind(performance.now()));
-  }
-  counter -= 100.0;
-  numberOfClicks[1]++;
-  cost[1] *= 1.15;
-  upgrade100.innerText = `Play a match of Valorant\nCost: ${roundHundredths(cost[1])}`;
-  updateButton();
-});
-upgrade1k.addEventListener("click", function () {
-  //Change growth rate. This is janky
-  for (let i = 0; i < 1000; i++) {
-    requestAnimationFrame(step.bind(performance.now()));
-  }
-  counter -= 1000.0;
-  numberOfClicks[2]++;
-  cost[2] *= 1.15;
-  upgrade1k.innerText = `Play a match of League of Legends\nCost: ${roundHundredths(cost[2])}`;
-  updateButton();
-});
+    counter -= item.cost;
+    item.cost *= 1.15;
+    item.button.innerText = `Play a match of ${item.name}\nCost: ${roundHundredths(item.cost)}`
+    updateButton();
+  });
+}
 
 //Helper function to round numbers to hundredths place and return string
 //Code for rounding decimal places found at: https://stackoverflow.com/a/5623156
@@ -90,18 +79,18 @@ function setupButton(upgrade: HTMLButtonElement, translateXPercent: string) {
 //Function to check if upgrade button has been clicked
 function checkUpgradeButton(upgrade: HTMLButtonElement, amount: number) {
   if (counter >= amount) {
-    upgrade.disabled = false; //Show button if counter is over 10
+    upgrade.disabled = false; //Show button if counter is over min amount
   } else {
     upgrade.disabled = true;
   }
 }
 
-//Helper function that updates the main button text
+//Helper function that updates the button
 function updateButton() {
   button.innerText = `🧂 Levels Increased By x ${roundHundredths(counter)}`;
-  checkUpgradeButton(upgrade10, cost[0]);
-  checkUpgradeButton(upgrade100, cost[1]);
-  checkUpgradeButton(upgrade1k, cost[2]);
+  for(const item of avaliableItems) {
+    checkUpgradeButton(item.button, item.cost);
+  }
 }
 
 //Function that starts gradual growth of the counter value
